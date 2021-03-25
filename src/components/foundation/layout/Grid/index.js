95434a12e6fd7de8e03aa7d992786064a1967685
Adoniro Salles <adonirosalles@gmail.com>
2021-03-25 13:37:21 -0300

    aprofundando-next

--- src/components/foundation/layout/Grid/index.js
+++ src/components/foundation/layout/Grid/index.js
@@ -8,7 +8,7 @@ const Container = styled.div`
   padding-left: 28px;
   margin-right: auto;
   margin-left: auto;
-  max-width: initial;
+  max-width: 100%;
   ${breakpointsMedia({
     sm: css`
       max-width: 576px;
